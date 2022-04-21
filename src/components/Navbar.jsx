dc1e50c5038515ded47d7154a09bf501462cb4f9
minhvu120201dn <minhvu120201dn.nkhv@gmail.com>
2022-04-21 17:09:45 +0700

    filter button added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { mobile } from "../responsive";
-
+import { Dropdown } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 const Container = styled.div`
     height: 50px;
     display: flex;
@@ -25,7 +26,17 @@ const NavItem = styled.div`
   ${mobile({ fontSize: "12px", marginLeft: "10px" })}
 `;
 
+const MyDrop = styled.div`
+    background-color: #ffc890;
+`;
+
 const Navbar = ({onChange}) => {
+
+    const customStyles = {
+        control: (base, state) => ({
+          background: "#ffc890",
+        }),
+      };
     return (
         <Container>
             <Center>
@@ -34,6 +45,40 @@ const Navbar = ({onChange}) => {
                 <NavItem onClick = {() => onChange('bistro')}> Quán ăn    </NavItem>
                 <NavItem onClick = {() => onChange('street')}> Đường phố    </NavItem>
                 <NavItem onClick = {() => onChange('cafe')}> Café/ Tráng miệng    </NavItem>
+                <NavItem>
+                    <Dropdown style={{backgroundColor: "#ffc890", borderStyle: "none", borderRadius: "20px"}} >              
+                        <Dropdown.Toggle style={{color: "black", backgroundColor: "#ffc890", borderStyle: "none" }}>
+                            Filter  
+                        </Dropdown.Toggle>
+    
+                        <Dropdown.Menu>
+                            <Dropdown.Item onClick = {() => onChange('#d1')}>Quận 1</Dropdown.Item>
+                            <Dropdown.Item onClick = {() => onChange('#d2')}>Quận 2</Dropdown.Item>
+                            <Dropdown.Item onClick = {() => onChange('#d3')}>Quận 3</Dropdown.Item>
+                            <Dropdown.Item onClick = {() => onChange('#d4')}>Quận 4</Dropdown.Item>
+                            <Dropdown.Item onClick = {() => onChange('#d5')}>Quận 5</Dropdown.Item>
+                            <Dropdown.Item onClick = {() => onChange('#d6')}>Quận 6</Dropdown.Item>
+                            <Dropdown.Item onClick = {() => onChange('#d7')}>Quận 7</Dropdown.Item>
+                            <Dropdown.Item onClick = {() => onChange('#d8')}>Quận 8</Dropdown.Item>
+                            <Dropdown.Item onClick = {() => onChange('#d9')}>Quận 9</Dropdown.Item>
+                            <Dropdown.Item onClick = {() => onChange('#d10')}>Quận 10</Dropdown.Item>
+                            <Dropdown.Item onClick = {() => onChange('#d11')}>Quận 11</Dropdown.Item>
+                            <Dropdown.Item onClick = {() => onChange('#d12')}>Quận 12</Dropdown.Item>
+                            <Dropdown.Item onClick = {() => onChange('#dbt')}>Quận Bình Thạnh</Dropdown.Item>
+                            <Dropdown.Item onClick = {() => onChange('#dtd')}>Tp.Thủ Đức</Dropdown.Item>
+                            <Dropdown.Item onClick = {() => onChange('#dgv')}>Quận Gò Vấp</Dropdown.Item>
+                            <Dropdown.Item onClick = {() => onChange('#dpn')}>Quận Phú Nhuận</Dropdown.Item>
+                            <Dropdown.Item onClick = {() => onChange('#dtb')}>Quận Tân Bình</Dropdown.Item>
+                            <Dropdown.Item onClick = {() => onChange('#dtp')}>Quận Tân Phú</Dropdown.Item>
+                            <Dropdown.Item onClick = {() => onChange('#dbt')}>Quận Bình Tân</Dropdown.Item>
+                            <Dropdown.Item onClick = {() => onChange('#dnb')}>Huyện Nhà Bè</Dropdown.Item>
+                            <Dropdown.Item onClick = {() => onChange('#dhm')}>Huyện Hóc Môn</Dropdown.Item>
+                            <Dropdown.Item onClick = {() => onChange('#dbc')}>Huyện Bình Chánh</Dropdown.Item>
+                            <Dropdown.Item onClick = {() => onChange('#dcc')}>Huyện Củ Chi</Dropdown.Item>
+                            <Dropdown.Item onClick = {() => onChange('#dcg')}>Huyện Cần Giờ</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </NavItem>
             </Center>
         </Container>
     )
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -26,8 +26,58 @@ const Products = ({value, option, itemDetail}) => {
         return data.filter((item) => item.category === 'Quán ăn');
       else if (option === 'street')
         return data.filter((item) => item.category === 'Ăn vặt/vỉa hè');
-      else
+      else if (option === 'cafe')
         return data.filter((item) => item.category === 'Café/Dessert');
+      else if (option[0] == '#') {
+        if (option === '#d1')
+          return data.filter((item) => item.address.split(", ").at(-2) === 'Quận 1');
+        else if (option ==='#d2')
+          return data.filter((item) => item.address.split(", ").at(-2) === 'Quận 2');
+        else if (option ==='#d3')
+          return data.filter((item) => item.address.split(", ").at(-2) === 'Quận 3');
+        else if (option ==='#d4')
+          return data.filter((item) => item.address.split(", ").at(-2) === 'Quận 4');
+        else if (option ==='#d5')
+          return data.filter((item) => item.address.split(", ").at(-2) === 'Quận 5');
+        else if (option ==='#d6')
+          return data.filter((item) => item.address.split(", ").at(-2) === 'Quận 6');
+        else if (option ==='#d7')
+          return data.filter((item) => item.address.split(", ").at(-2) === 'Quận 7');
+        else if (option ==='#d8')
+          return data.filter((item) => item.address.split(", ").at(-2) === 'Quận 8');
+        else if (option ==='#d9')
+          return data.filter((item) => item.address.split(", ").at(-2) === 'Quận 9');
+        else if (option ==='#d10')
+          return data.filter((item) => item.address.split(", ").at(-2) === 'Quận 10');
+        else if (option ==='#d11')
+          return data.filter((item) => item.address.split(", ").at(-2) === 'Quận 11');
+        else if (option ==='#d12')
+          return data.filter((item) => item.address.split(", ").at(-2) === 'Quận 12');
+        else if (option ==='#dbt')
+          return data.filter((item) => item.address.split(", ").at(-2) === 'Quận Bình Thạnh');
+        else if (option ==='#dtd')
+          return data.filter((item) => item.address.split(", ").at(-2) === 'Tp. Thủ Đức');
+        else if (option ==='#dgv')
+          return data.filter((item) => item.address.split(", ").at(-2) === 'Quận Gò Vấp');
+        else if (option ==='#dpn')
+          return data.filter((item) => item.address.split(", ").at(-2) === 'Quận Phú Nhuận');
+        else if (option ==='#dtb')
+          return data.filter((item) => item.address.split(", ").at(-2) === 'Quận Tân Bình');
+        else if (option ==='#dtp')
+          return data.filter((item) => item.address.split(", ").at(-2) === 'Quận Tân Phú');
+        else if (option ==='#dbt')
+          return data.filter((item) => item.address.split(", ").at(-2) === 'Quận Bình Tân');
+        else if (option ==='#dnb')
+          return data.filter((item) => item.address.split(", ").at(-2) === 'Huyện Nhà Bè');
+        else if (option ==='#dhm')
+          return data.filter((item) => item.address.split(", ").at(-2) === 'Huyện Hóc Môn');
+        else if (option ==='#dbc')
+          return data.filter((item) => item.address.split(", ").at(-2) === 'Huyện Bình Chánh');
+        else if (option ==='#dcc')
+          return data.filter((item) => item.address.split(", ").at(-2) === 'Huyện Củ Chi');
+        else if (option ==='#dcg')
+          return data.filter((item) => item.address.split(", ").at(-2) === 'Huyện Cần Giờ');
+      }
     }
     // Need to check for searched value
     else{
@@ -39,8 +89,8 @@ const Products = ({value, option, itemDetail}) => {
         return data.filter((item) => item["restaurant"].toLowerCase().includes(value.toLowerCase()) && item.category === 'Quán ăn');
       else if (option === 'street')
         return data.filter((item) => item["restaurant"].toLowerCase().includes(value.toLowerCase()) && item.category === 'Ăn vặt/vỉa hè');
-      else
-      return data.filter((item) => item["restaurant"].toLowerCase().includes(value.toLowerCase()) && item.category === 'Café/Dessert');
+      else if (option === 'cafe')
+        return data.filter((item) => item["restaurant"].toLowerCase().includes(value.toLowerCase()) && item.category === 'Café/Dessert');
     }
   }
 
